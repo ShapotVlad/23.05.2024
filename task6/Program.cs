b1a8a4937ae6b497d7b6a899096cb5ca5e682a83
Vladimir <vlnshapat@gmail.com>
2024-05-23 23:14:52 +0300

    add task6

---
+++ task6/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца.
+// Использовать рекурсию, не использовать циклы
+
+void PrintArrayReverse(int[] arr, int index)
+    {
+        if (index >= 0)
+        {
+            Console.Write(arr[index] + " ");
+            PrintArrayReverse(arr, index - 1);
+        }
+    }
+
+    
+    
+        int[] myArray = { 10, 20, 30, 40, 50 }; // Произвольный массив
+
+        Console.WriteLine("Элементы массива в обратном порядке:");
+        PrintArrayReverse(myArray, myArray.Length - 1);
+    
+
