21b8509c2481071c0e8b4398615b1fe6bbd4a5ee
Vladimir <vlnshapat@gmail.com>
2024-05-23 22:42:28 +0300

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// факториал числа
+ // int Factorial(int n)
+// {
+// 	if(n == 1) return 1;
+// 	return n * Factorial(n - 1);
+// }
+
+// Console.WriteLine(Factorial(5)); // 120
+
+// Напишите программу, которая будет принимать на
+// вход число и возвращать сумму его цифр.
+// Указание
+// Использовать рекурсию.
+// Пример
+// 123 => 6
+// 63 => 9
+
+int SumDigits(int num) // 123, 12, 1, 0
+{
+    if (num == 0) return 0; // 
+    return num % 10 + SumDigits(num / 10); //123, 12, 1
+    // 1 % 10 => 1 + 12 % 10 => 2 + 123 % 10 => 3  =  1+2+3 = 6+0
+}
+
+Console.WriteLine(SumDigits(128573));
