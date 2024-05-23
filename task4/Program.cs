b89a30738a2a4595a8e126b8f633615060d010b8
Vladimir <vlnshapat@gmail.com>
2024-05-23 22:53:32 +0300

    add task4

---
+++ task4/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+
+  void PrintNaturalNumbers(int M, int N)
+    {
+        if (M <= N)
+        {
+            Console.Write(M + " ");
+            PrintNaturalNumbers(M + 1, N);
+        }
+    }
+
+    
+    
+        Console.Write("Введите значение M: ");
+        int M = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Натуральные числа от {M} до {N}:");
+        PrintNaturalNumbers(M, N);
+    
