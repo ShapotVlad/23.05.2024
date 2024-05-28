01ab6e6dc863014e18f1a25a3c238f3035d570aa
Vladimir <vlnshapat@gmail.com>
2024-05-28 22:18:37 +0300

    add explanations

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,34 +1,34 @@
-﻿Считать строку с консоли, содержащую латинские
-буквы. Вывести на экран согласные буквы этой
-строки.
-Указание
-Использовать рекурсию. Не использовать цикл.
-Пример
-“hello” => h l l
-“World” => W r l d
-“Hello world!” => H l l w r l d
+﻿// Считать строку с консоли, содержащую латинские
+// буквы. Вывести на экран согласные буквы этой
+// строки.
+// Указание
+// Использовать рекурсию. Не использовать цикл.
+// Пример
+// “hello” => h l l
+// “World” => W r l d
+// “Hello world!” => H l l w r l d
 
 
-static void CheckPrintConsonats(string txt, int index)
+ void CheckPrintConsonats(string txt, int index)  //создали функцию выборки  главсных из текста
 {
     string vowels = "aeiouy";
-    if (char.IsAsciiLetter(txt[index]) && !vowels.Contains(txt[index]))
+    if (char.IsAsciiLetter(txt[index]) && !vowels.Contains(txt[index])) //отсекает  символы  цифры и гласные
     {
         Console.Write($"{txt[index]}");
     }
 }
 
-void PrintConsonants(string txt, int index = 0)
+void PrintConsonants(string txt, int index = 0) // создали  функцию вывода на консоль гласных из текста
 {
     if (index == txt.Length)
     {
         return;
     }
-    CheckPrintConsonats(txt, index);
-    PrintConsonants(txt, index + 1);
+    CheckPrintConsonats(txt, index); //вызов функции выборки
+    PrintConsonants(txt, index + 1); // рекурсия функции
 }
 
 Console.WriteLine("введите строку содержащую латинские буквы: ");
 string text = Console.ReadLine();
 Console.WriteLine();
-PrintConsonants(text);
+PrintConsonants(text); // вызов функции
