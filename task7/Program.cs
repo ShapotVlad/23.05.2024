a41f9894b7940ca60f42f5067cf2563a01112704
VladShapot <vlnshapot@gmail.com>
2024-06-11 14:02:59 +0300

    добавили изменения

--- task7/Program.cs
+++ task7/Program.cs
@@ -35,3 +35,42 @@ Console.WriteLine($" => {result}");
 
 
 
+
+
+//метод для рекурсивного бинарного поиска
+    int BinarySearch1(int[] array, int searchedValue, int first, int last)
+    {
+        //границы сошлись
+        if (first > last)
+        {
+            //элемент не найден
+            return -1;
+        }
+
+        //средний индекс подмассива
+        var middle = (first + last) / 2;
+        //значение в средине подмассива
+        var middleValue = array[middle];
+
+        if (middleValue == searchedValue)
+        {
+            return middle;
+        }
+        else
+        {
+            if (middleValue > searchedValue)
+            {
+                //рекурсивный вызов поиска для левого подмассива
+                return BinarySearch1(array, searchedValue, first, middle - 1);
+            }
+            else
+            {
+                //рекурсивный вызов поиска для правого подмассива
+                return BinarySearch1(array, searchedValue, middle + 1, last);
+            }
+        }
+    } 
+int[] arr1 = { 1, 3, 5, 7, 9, 11, 13};//создали переменный цельный массив arr ОТСОРТИРОВАННЫЙ
+
+int result1 = BinarySearch1(arr1, 13, 0, 6);         //вызов функции  бинарного поиска и вывод числа на  консоль  
+Console.WriteLine($" => {result1}");
